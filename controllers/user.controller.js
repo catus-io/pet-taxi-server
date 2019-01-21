05a5ee045bea1883da9a9a632729f502b0ab22cb
Jny <nerdrun.jny@gmail.com>
2019-01-21 11:29:24 +0900

    #1 signup ing

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,26 @@
+import * as Users from '../models/Users'
 const create = (req, res) => {
-  console.log('hello im create') 
+  const { email, nickname, password } = req.body
+  const createdDateAt = Date.now()
+  
+  
+  Users.insertUser(email, nickname, password, createdDateAt)
+  .then(result => {
+    // console.log(result)
 
+  })
+/** 처리 해야할 사안
+  const addUser = user => {
+    if(user) throw new Error('user already exists')
+    return User.insertUser(id, password)
+  }
+  const respond = response => res.json({success: response.result.n})
+  const onError = () => res.status(409).json({success: 0})
+  Users.findUserByEmail(email)
+  .then(addUser)
+  .then(respond)
+  .catch(onError)
+  */
 }
 
 const login = (req, res) => {
---
+++ models/Users.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require('mongodb')
+const url = 'mongodb://catus:catus1!@ds021016.mlab.com:21016/pettaxi'
+const dbName = 'pettaxi'
+const colName = 'users'
+
+const findUserByEmail = email => {
+  return MongoClient.connect(url, { useNewUrlParser: true })
+  .then(client => {
+    const col = client.db(dbName).collection(colName);
+    try {
+      return col.findOne({email: email})
+    }
+    finally {
+      client.close()
+    }
+  })
+}
+const insertUser = (email, nickname, password, createdDateAt) => {
+  return MongoClient.connect(url, { useNewUrlParser: true })
+  .then(client => {
+    const col = client.db(dbName).collection(colName);
+    try {
+      return col.insertOne({email: email, nickname: nickname, password: password, createdDateAt: createdDateAt})
+    }
+    finally {
+      client.close()
+    }
+  })
+}
+
+export { findUserByEmail, insertUser }
